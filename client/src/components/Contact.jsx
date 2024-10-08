3df2dd9d928ec358003eb17b0f1b90f68fa9c091
unknown <vadakapuramkalyani@gmail.com>
2024-10-08 19:39:20 +0530

    contact.jsx modified

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+//this is new modified code
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
   const [message, setMessage] = useState('');
